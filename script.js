37fbf9b270443fd2f3dbb89eda1dc57a2e52eaf7
shekhargrace14 <shekhargrace14@gmail.com>
2024-11-10 01:39:36 +0530

    news added

--- script.js
+++ script.js
@@ -43,13 +43,15 @@ fetch("./blog.json")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    data.forEach((e) => {
+    data.slice(4).forEach((e) => {
         blog.insertAdjacentHTML("beforeend", `
         <div class="column">
-        <div class="details">
-        <img src="${e.image}" alt="">
+          <div class="details">
+            <div class="image">
+              <img src="${e.image}" alt="">
+            </div>
         <h3>${e.title} </h3>
-        <p>${e.title}</p>
+        <p>${e.content}</p>
         <p>${e.venue} </p>
         </div>
         
@@ -62,3 +64,31 @@ fetch("./blog.json")
 
 
 // blog ends here 
+// news  starts here 
+
+let news  = document.getElementById("news");
+
+fetch("./blog.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((e) => {
+      news.insertAdjacentHTML("beforeend", `
+        <div class="column">
+        <div class="image">
+        <img src="${e.image}" alt="">
+        <div class="overlay"></div>
+        </div>
+        <div class="details">
+        <p>Trending</p>
+        <h3>${e.title} </h3>
+        <p>${e.venue} </p>
+        </div> 
+        </div>
+        `)
+    })
+} )
+.catch((error) => console.error("Error fetching data:", error));
+
+
+// news ends here 
